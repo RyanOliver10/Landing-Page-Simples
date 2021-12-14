11ee714a8b14a3f8d3daca79c03e59dd7f97bc27
Ryan Oliveira <84991377+RyanOliver10@users.noreply.github.com>
2021-12-14 16:54:42 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area-1', {duration: 1000});
+sr.reveal('.area-2', {
+    rotate: {x: 0, y: 80, z:0},
+    duration: 2000});
+sr.reveal('.area-3', {
+    rotate: {x: 80, y: 0, z:0},
+    duration: 2500});
